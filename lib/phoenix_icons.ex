424c660f6fa00020c69092b97b5b6175efa9c60a
Mario Uher <uher.mario@gmail.com>
2026-04-15 13:49:04 +0200

    Initialize repo

---
+++ lib/phoenix_icons.ex
@@ -0,0 +1,18 @@
+defmodule PhoenixIcons do
+  @moduledoc """
+  Documentation for `PhoenixIcons`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhoenixIcons.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phoenix_icons_test.exs
@@ -0,0 +1,8 @@
+defmodule PhoenixIconsTest do
+  use ExUnit.Case
+  doctest PhoenixIcons
+
+  test "greets the world" do
+    assert PhoenixIcons.hello() == :world
+  end
+end
